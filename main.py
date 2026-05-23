7c81992b981cec586609d629968c8eca774bdc19
SiFrancis <franzo08112007@gmail.com>
2026-05-23 17:53:26 +0800

    adding window

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+import pygame
+
 def main():
-    print("Hello from snake-game!")
-    print('helloo')
+    pygame.init()
+    window = pygame.Window(size=(640, 480))
+    window.title = "Simple Snake Game"
+    screen = window.get_surface()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 200))
+        window.flip()
+    
+    pygame.quit()
 
 
 if __name__ == "__main__":
